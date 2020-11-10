9c558a2d8903dadecc2e8d0237c01add04306050
Marina Labutina <63440386+MarinaLabutina@users.noreply.github.com>
2020-11-09 23:18:39 -0500

    added solutions for new tasks

---
+++ src/Cube.java
@@ -0,0 +1,17 @@
+public class Cube {
+    //  Playing with cubes I
+    // https://www.codewars.com/kata/55c0a79e20be94c91400014b/solutions/java
+
+    private int Side;
+
+    // Get method
+    public int getSide() {
+        return Side;
+    }
+
+    //set method
+    public void setSide(int num){
+        this.Side = num;
+    }
+
+}
