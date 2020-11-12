9d3de1c258888d8bccc277ff2ac38d57d39c3e3a
Marina Labutina <63440386+MarinaLabutina@users.noreply.github.com>
2020-11-11 22:34:41 -0500

    added solutions for new tasks

---
+++ src/Solution1.java
@@ -0,0 +1,9 @@
+public class Solution1 {
+   // Convert a string to an array;
+    // https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/java
+
+    public static String[] stringToArray(String s) {
+        String[] words=s.split(" ");
+        return words;
+    }
+}
